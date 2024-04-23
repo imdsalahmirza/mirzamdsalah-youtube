cd283a8c69a757fbb506d0e160f3fbcd1a9be433
Salah mirza <106913322+imdsalahmirza@users.noreply.github.com>
2024-04-23 18:01:40 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "33abc"
+
+console.log(typeof score);
+
+let valueInNumber  = Number(score)
+console.log (typeof valueInNumber)
+console.log (valueInNumber)
+
+// nan = not a number
+// null = 0
+//"33" = 33
+// "3abc" = nan
+// true = 1 false = 0
+
+let isLoggedIn = 1  
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log (booleanIsLoggedIn)
+
+// boolean 
+// 1 = true 0 = false 
+// "" = false 
+// salah=true
+
+let somenumber = 33 
+let stringNumber = String(somenumber)
+console.log (typeof stringNumber)

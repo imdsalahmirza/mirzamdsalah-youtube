690429faaf893c9e09b5e2995c7c2db95af6bbd8
Salah mirza <106913322+imdsalahmirza@users.noreply.github.com>
2024-04-23 16:54:10 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3 + 3) // we are using node js not browser
+
+console.log(3 + 3)  // code readability should be high
+
+console.log("salah")
+
+
+let name = "salah" // string
+let age = 22   // number
+let isloggedIn = false  // boolean
+
+
+// data types
+
+// number => 2 to power 53
+// bigInt (zyada number)
+// string => ""
+// boolean => true or false
+// null (standalone value) empty value = not undefined its empty
+// undefined =>
+// symbol => uniqueness
+
+
+// object 
+
+console.log(typeof null);  // object
+console.log(typeof undefined);  //  undefined

36b2e6e86681a3cd620be23d10c0ad6856313a20
Salah mirza <106913322+imdsalahmirza@users.noreply.github.com>
2024-04-24 17:39:27 +0000

    conversion/Operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,10 +1,10 @@
-let score = "33abc"
+// let score = "33abc"
 
-console.log(typeof score);
+// console.log(typeof score);
 
-let valueInNumber  = Number(score)
-console.log (typeof valueInNumber);
-console.log (valueInNumber);
+// let valueInNumber  = Number(score)
+// console.log (typeof valueInNumber);
+// console.log (valueInNumber);
 
 // nan = not a number
 // null = 0
@@ -13,18 +13,58 @@ console.log (valueInNumber);
 // true = 1 false = 0
 // undefined = nan
 
-let isLoggedIn = 1
+// let isLoggedIn = 1
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log ( booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log ( booleanIsLoggedIn);
 
 // boolean //
 // 1 = true 0 = false 
 // "" = false 
 // salah=true
 
-let somenumber = "33" 
-let stringNumber = String(somenumber)
-console.log (stringNumber);
-console.log (typeof stringNumber);
+// let somenumber = "33" 
+// let stringNumber = String(somenumber)
+// console.log (stringNumber);
+// console.log (typeof stringNumber); 
 
+// *************************************** Operations *******************************************
+
+let value = 3
+let negvalue = -value
+// console.log (negvalue);
+
+// console.log (2+2);
+//console.log (2-2);
+//console.log (2*2);
+//console.log (2**2);
+//console.log (2**3);
+//console.log (2/2);
+//console.log (2%2);
+
+let str1 = "hello"
+let str2 = " salah"
+let str3 = str1 + str2
+console.log (str3)
+
+console.log ("1" + 2);
+console.log (1 + "2");
+console.log ("1" + 2 + 2);
+console.log (1 + 2 + "2");
+
+console.log ( (3 + 4) * 5 % 3)
+
+console.log (+true);
+console.log (+"")  
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gamecounter = 100
+gamecounter++;
+console.log (gamecounter);
+
+// link to study 
+// study conversion ecma
+// prefix postfix js mdn

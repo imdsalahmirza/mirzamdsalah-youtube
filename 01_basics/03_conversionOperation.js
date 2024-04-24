d005c46cb3d75da77bcfbfc5d876ba3e5cb5fed6
Salah mirza <106913322+imdsalahmirza@users.noreply.github.com>
2024-04-24 16:41:56 +0000

    conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -3,25 +3,28 @@ let score = "33abc"
 console.log(typeof score);
 
 let valueInNumber  = Number(score)
-console.log (typeof valueInNumber)
-console.log (valueInNumber)
+console.log (typeof valueInNumber);
+console.log (valueInNumber);
 
 // nan = not a number
 // null = 0
 //"33" = 33
 // "3abc" = nan
 // true = 1 false = 0
+// undefined = nan
 
-let isLoggedIn = 1  
+let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log (booleanIsLoggedIn)
+console.log ( booleanIsLoggedIn);
 
-// boolean 
+// boolean //
 // 1 = true 0 = false 
 // "" = false 
 // salah=true
 
-let somenumber = 33 
+let somenumber = "33" 
 let stringNumber = String(somenumber)
-console.log (typeof stringNumber)
+console.log (stringNumber);
+console.log (typeof stringNumber);
+

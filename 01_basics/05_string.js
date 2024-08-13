119295d152af7712037c3d0272eb2b07e7147e3a
Salah mirza <106913322+imdsalahmirza@users.noreply.github.com>
2024-08-13 05:36:05 +0000

    modern string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,20 @@
+const name = "salah"
+const repocount = 3
+
+//console.log(name + repocount +" value") 
+// this is not good 
+
+console.log(`hello my name is ${name} & my repo count is ${repocount} `);
+
+// modern way of syntax and it is call string interpollation 
+
+const gamename = new String("salahhc")
+
+console.log( gamename[0])
+console.log( gamename.__proto__)
+console.log( gamename)
+console.log( gamename.length)
+console.log( gamename.padEnd())
+console.log( gamename.toUpperCase())
+console.log( gamename.charAt(2))
+console.log( gamename.indexOf('s'))

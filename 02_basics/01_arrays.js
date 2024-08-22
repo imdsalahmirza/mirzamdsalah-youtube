07b2d00948a36dfaebe79e2cee2e8b4ecc671476
Salah mirza <106913322+imdsalahmirza@users.noreply.github.com>
2024-08-22 06:16:45 +0000

    Array

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -21,8 +21,8 @@
 // deep-copy=== properties do not share the same reference
 
 
-const myArr = [0, 1, 2, 3, 4, 5, ]
+const myArr = [0, 1, 2, 3, 4, 5]
 const maulanas = ["nusrat bukhari", "alirazarizvi"]
 
 const myArr2 = new Array(1, 2, 3, 4)
-console.log(myArr[0]);
+console.log(myArr[0])

a3c0e5643661be6e565d6f586e82895baed09b69
Salah mirza <106913322+imdsalahmirza@users.noreply.github.com>
2024-10-13 15:39:51 +0000

    json talks

--- 02_basics/05_objects.js
+++ 02_basics/05_objects.js
@@ -11,3 +11,29 @@ const course = {
 
 const {courseinstructor: instructor} = course
 console.log(instructor)                             // this is obj destructure
+
+
+
+// const navbar = ({company}) => {         // by defaul props. for ex props.company mostly people put curly braces in function
+                               // parenthises instead of props.
+// }
+                                 // this is destructure method in react not javacript
+// navbar(company = "codeuniverse")
+
+// APIs concept = when you give work to someone else is called APIs. 
+
+//          -------------------------------------json-------------------------------------------
+
+// {
+//     "name": "hitesh",
+//     "coursename": "js hindi",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
+
+// jason is java script object nation this is not limited only in javascript it is works on all over APIs industry.

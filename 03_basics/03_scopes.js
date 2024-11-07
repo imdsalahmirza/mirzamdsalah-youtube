2cc29a6ebecf89090d0c59d7759d54426c6f0ef1
Salah mirza <106913322+imdsalahmirza@users.noreply.github.com>
2024-11-07 05:18:29 +0000

    basics scope

--- 03_basics/03_scopes.js
+++ 03_basics/03_scopes.js
@@ -1,15 +1,24 @@
 
 // {}                this curly braces called scopes almost in all prgramming language
                 //  But in object {} this is not scope thats object decleration.
+// var c = 300
+// if (true){
+//     let a = 30
+//     const b = 40
+//      c = 50
+// }
 
+let a = 300
 if (true){
     let a = 30
     const b = 40
-    var c = 50
-
+    console.log("inner: ",a);   // outside the scope is global scope and inside is block scope
+    
 }
 
 
 console.log(a);
-console.log(b);
-console.log(c);
+// console.log(b);
+// console.log(c);
+
+// global scope in brower is different than  global scope in node

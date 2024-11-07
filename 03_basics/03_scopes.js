f4054d56b761712f07dd5054bc5d81a719a576d7
Salah mirza <106913322+imdsalahmirza@users.noreply.github.com>
2024-11-07 03:59:24 +0000

    basics scopes

---
+++ 03_basics/03_scopes.js
@@ -0,0 +1,15 @@
+
+// {}                this curly braces called scopes almost in all prgramming language
+                //  But in object {} this is not scope thats object decleration.
+
+if (true){
+    let a = 30
+    const b = 40
+    var c = 50
+
+}
+
+
+console.log(a);
+console.log(b);
+console.log(c);

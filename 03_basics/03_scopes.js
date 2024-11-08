9a498487338d8c0a26c4bbc8f3e810a7c0982f3c
Salah mirza <106913322+imdsalahmirza@users.noreply.github.com>
2024-11-08 17:27:30 +0000

    basic hoisting

--- 03_basics/03_scopes.js
+++ 03_basics/03_scopes.js
@@ -12,14 +12,71 @@ let a = 300
 if (true){
     let a = 30
     const b = 40
-    console.log("inner: ",a);   // outside the scope is global scope and inside is block scope
+    // console.log("inner: ",a);   // outside the scope is global scope and inside is block scope
     
 }
 
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
 
 // global scope in brower is different than  global scope in node.....
 // global scope value can be available under the scope but inner scope should not go outside the scope..
+
+
+// *********************************************************************************************
+ 
+// nested scope
+
+// child function can exces parent variable but child variable should not be axces by parent function
+
+
+function one(){
+    const username = "salah"
+
+    function two(){
+        const website = "youtube"
+        // console.log(username)
+          
+    }
+
+    // console.log(website);
+    two()   
+}
+
+one()
+
+if (true) {
+    const username = "salah"
+
+    if (username === "salah"){
+        const website = "youtube"
+        // console.log(username +  website);   
+        // console.log(website);
+         
+    }
+    // console.log(website);
+    // console.log(username);
+    
+}
+
+// console.log(username);
+
+
+// ****************************INTERESTING*******************************
+
+console.log(addone(10))
+
+function addone (num) {
+    return num + 1
+}
+ 
+
+const addtwo = function(num){
+    return num + 9
+}
+
+console.log(addtwo(6))
+
+
